9e88cb938ff5ebdbcf1dda544f27a0ef13d1ae59
vishnupriya111220 <82633192+vishnupriya111220@users.noreply.github.com>
2021-08-16 21:24:10 +0530

    Create max.java

---
+++ max.java
@@ -0,0 +1,11 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    int diff=23,peri=206,area;
+	    int sumoflb=peri/2;
+	    int dlength=sumoflb+diff;
+	    int len=dlength/2;
+	    int breath=sumoflb-len;
+		System.out.println(len*breath);
+	}
+}
